8a2745e982774d0566c968f43e39bc6a1118e838
Cady29 <caddyshabangu@gmail.com>
2025-03-12 09:50:52 +0200

    Project

--- AccountManagement1/src/main/java/com/cady/AccountManagement/Repository/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cady.AccountManagement.Repository;
-
-import com.cady.AccountManagement.Entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account ,Long> {
-
-}
